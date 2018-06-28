f666ec98273ae0adc8c4131ebece4577669623dc
Leonardo <leonardo.capoccia@hotmail.it>
2018-06-28 10:59:20 +0200

    Add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,18 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: '**', component: LoginComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,9 @@ import { UserService } from '../../services/user.service';
 import { UserCredentials } from '../../models/UserCredentials';
 import { User } from '../../models/User';
 
+import { LocalStorage } from '@ngx-pwa/local-storage';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -15,17 +18,38 @@ export class LoginComponent implements OnInit {
     username: '',
     password: ''
   };
+  showError = false;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private localStorage: LocalStorage,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.localStorage.getItem('loggedUser').subscribe(user => {
+      if (user) {
+        this.goToHome();
+      }
+    });
   }
 
   formSubmitted() {
     this.userService.checkCredentials(this.userCredentials)
       .subscribe((user: User) => {
         console.log(user);
+        if (user) {
+          this.localStorage.setItem('loggedUser', user).subscribe(() => {
+            this.goToHome();
+          });
+        }
+      }, error => {
+        this.showError = true;
       });
+  }
+
+  goToHome(): void {
+    this.router.navigateByUrl('home');
   }
 
 }
