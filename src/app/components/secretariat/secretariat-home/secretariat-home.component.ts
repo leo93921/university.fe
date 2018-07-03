ea991d893da74ecac023e3d88c000fd30e9c97bc
Leonardo <leonardo.capoccia@hotmail.it>
2018-07-03 12:04:45 +0200

    Add logout

--- src/app/components/secretariat/secretariat-home/secretariat-home.component.ts
+++ src/app/components/secretariat/secretariat-home/secretariat-home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../../models/User';
 import { SecretariatService } from '../../../services/secretariat.service';
 import { CourseOfStudyService } from '../../../services/course-of-study.service';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-secretariat-home',
@@ -14,10 +16,18 @@ export class SecretariatHomeComponent implements OnInit {
   private loggedUser: User;
 
   constructor(
-    public secretariatService: SecretariatService
+    public secretariatService: SecretariatService,
+    private localStorage: LocalStorage,
+    private router: Router
   ) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.localStorage.removeItem('loggedUser').subscribe(() => {
+      this.router.navigateByUrl('/login');
+    });
   }
 
 }
